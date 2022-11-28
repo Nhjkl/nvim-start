b9545f1a58f800c0e42fe592f4e6b343640a27ab
liuxian <liuxiangogo@gmail.com>
2022-11-28 22:08:25 +0800

    feat: 添加 bufferline 插件

---
+++ lua/plugins/windows/bufferline.lua
@@ -0,0 +1,37 @@
+return {
+  'akinsho/bufferline.nvim',
+
+  tag = "v2.*",
+
+  event = 'BufReadPre',
+
+  config = function ()
+    vim.opt.termguicolors = true
+
+    require("bufferline").setup({
+      options = {
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "Explorer",
+            highlight = "PanelHeading",
+            padding = 1,
+          },
+        },
+      }
+    })
+
+    function _G.BufferCloseAllButCurrent()
+      vim.cmd('BufferLineCloseLeft')
+      vim.cmd('BufferLineCloseRight')
+    end
+
+    Utils.Shared.addGlobalKeybinds({
+      { 'n', '<tab>'    , ':BufferLineCycleNext<cr>'       , 'ns' },
+      { 'n', '<s-tab>'  , ':BufferLineCyclePrev<cr>'       , 'ns' },
+      { 'n', '<leader>0', ':lua BufferCloseAllButCurrent()<cr>'  , 'ns' },
+      { 'n', '<leader>k', ':BufferLinePickClose<cr>'        , 'ns' },
+      -- { 'n', '<leader>w', ':BufferClose<cr>'               , 'ns' },
+    })
+  end
+}
